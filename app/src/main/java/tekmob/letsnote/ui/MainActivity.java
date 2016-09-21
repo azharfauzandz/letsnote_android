d17d05bdcbe875f5aa8339656b7e213b8358dab4
Feaza Nurrachman <feaza.nurrachman@gmail.com>
2016-09-21 13:18:50 +0700

    added folders

---
+++ app/src/main/java/tekmob/letsnote/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package tekmob.letsnote.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tekmob.letsnote.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
