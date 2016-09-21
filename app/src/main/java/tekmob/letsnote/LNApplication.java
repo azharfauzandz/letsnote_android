1831238be347aa47a7e524e75b9c75528fbbb8dd
Feaza Nurrachman <feaza.nurrachman@gmail.com>
2016-09-21 13:31:16 +0700

    added application file, added library

---
+++ app/src/main/java/tekmob/letsnote/LNApplication.java
@@ -0,0 +1,14 @@
+package tekmob.letsnote;
+
+import android.app.Application;
+
+/**
+ * Created by feaza on 9/21/2016.
+ * Initialize and register managers here
+ */
+public class LNApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
