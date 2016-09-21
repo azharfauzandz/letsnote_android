2f68a91c708aee223ef67994d455597d249c36da
Feaza Nurrachman <feaza.nurrachman@gmail.com>
2016-09-21 14:16:44 +0700

    added client

---
+++ app/src/main/java/tekmob/letsnote/network/LNClient.java
@@ -0,0 +1,33 @@
+package tekmob.letsnote.network;
+
+import com.squareup.okhttp.OkHttpClient;
+
+import retrofit.RestAdapter;
+import retrofit.client.OkClient;
+
+/**
+ * Created by feaza on 9/21/2016.
+ */
+public class LNClient {
+    private static LNClient lnClient;
+    private RestAdapter restAdapter;
+
+    private LNClient() {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        restAdapter = new RestAdapter.Builder()
+                .setEndpoint("insert API URL here")
+                .setClient(new OkClient(okHttpClient))
+                .setLogLevel(RestAdapter.LogLevel.FULL)
+                .build();
+    }
+
+    public static LNClient getClient() {
+        if(lnClient == null)
+            lnClient = new LNClient();
+        return lnClient;
+    }
+
+    public RestAdapter getRestAdapter() {
+        return restAdapter;
+    }
+}
