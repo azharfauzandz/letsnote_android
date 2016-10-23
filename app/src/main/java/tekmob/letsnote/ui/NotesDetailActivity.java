bfce083adefd4382442ea398de0e53d23e558742
Feaza Nurrachman <feaza.nurrachman@gmail.com>
2016-10-23 17:42:52 +0700

    added image to notes detail

--- app/src/main/java/tekmob/letsnote/ui/NotesDetailActivity.java
+++ app/src/main/java/tekmob/letsnote/ui/NotesDetailActivity.java
@@ -3,8 +3,12 @@ package tekmob.letsnote.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -16,6 +20,7 @@ import tekmob.letsnote.events.GetNotesEvent;
 import tekmob.letsnote.events.GetNotesResultEvent;
 import tekmob.letsnote.helper.LNSession;
 import tekmob.letsnote.models.GetNotesResultModel;
+import tekmob.letsnote.network.VolleySingleton;
 
 public class NotesDetailActivity extends BaseActivity {
     @Bind(R.id.notes_title)
@@ -28,12 +33,19 @@ public class NotesDetailActivity extends BaseActivity {
     TextView notesLink;
     @Bind(R.id.notes_usercoin)
     TextView notesUsercoin;
+    @Bind(R.id.notes_image)
+    ImageView notesImage;
+
+    private VolleySingleton volleySingleton;
+    private ImageLoader imageLoader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_detail);
         ButterKnife.bind(this);
+        volleySingleton=VolleySingleton.getsInstance();
+        imageLoader=volleySingleton.getImageLoader();
         LNSession session = new LNSession(getApplicationContext());
         String idNotes = getIntent().getStringExtra("NOTES_ID");
         String idUser = session.getUserId();
@@ -44,17 +56,35 @@ public class NotesDetailActivity extends BaseActivity {
     public void onEvent(GetNotesResultEvent event) {
         Log.d("event", "get notes result");
         hideDialog();
-        if(event.getStatus() == event.OK) {
+        if (event.getStatus() == event.OK) {
             GetNotesResultModel model = event.getGetNotesResultModel();
             notesTitle.setText(model.getTitle());
             notesDescription.setText(model.getDescription());
             notesPrice.setText(model.getPrice());
             notesLink.setText(model.getPhotoLink());
             notesUsercoin.setText(model.getCoinBalance());
-        }
-        else {
-            Toast.makeText(this, "get failed", Toast.LENGTH_SHORT).show();
-        }
+            if (model.getPhotoLink() != null) {
+                imageLoader.get(model.getPhotoLink(), new ImageLoader.ImageListener() {
 
+                    @Override
+                    public void onResponse(ImageLoader.ImageContainer response, boolean isImmediate) {
+                        Log.d("masuk on response", "a");
+                        notesImage.setImageBitmap(response.getBitmap());
+                    }
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("masuk error response", "a");
+                        //Log.d("eror gambar",error.getMessage());
+                        notesImage.setImageResource(R.mipmap.ic_launcher);
+                    }
+
+
+                });
+            } else {
+                Toast.makeText(this, "get failed", Toast.LENGTH_SHORT).show();
+            }
+
+        }
     }
 }
